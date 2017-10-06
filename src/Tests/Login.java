f638499b1f29436a738e1b9164c1aadc8273a6a7
NTQ-PC\NTQ <luonghongthao92@gmail.com>
2017-10-06 16:56:07 +0700

    Commit: - Change Login.java - Change HomePage.java

--- src/Tests/Login.java
+++ src/Tests/Login.java
@@ -22,69 +22,87 @@ public class Login {
 		driver.get("http://192.168.7.43:9001/#/");
 		driver.manage().window().maximize();
 		Thread.sleep(30000);
-		HomePage home = new HomePage(driver);
-		home.account();
-		home.login();
+//		HomePage home = new HomePage(driver);
+//		home.account();
+//		home.login();
+		HomePage.account();
+		HomePage.login();
 	}
 
 	@Test(description = "Login fail with blank acc and pw")
 	public void Blank_acc_pw() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		HomePage home = new HomePage(driver);
-		home.submit();
+//		HomePage home = new HomePage(driver);
+//		home.submit();
+		HomePage.submit();
 //		String mess = null;
 //		mess = home.getMessage1();
 //		System.out.println(mess);
-		AssertJUnit.assertEquals(home.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
+		AssertJUnit.assertEquals(HomePage.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
 	}
 	
 	@Test(description = "Login fail with blank acc")
 	public void Blank_acc() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		HomePage home = new HomePage(driver);
-		home.typeUsername("");
-		home.typePassword("admin");
-		home.submit();
-		AssertJUnit.assertEquals(home.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
+//		HomePage home = new HomePage(driver);
+//		home.typeUsername("");
+//		home.typePassword("admin");
+//		home.submit();
+		HomePage.typeUsername("");
+		HomePage.typePassword("admin");
+		HomePage.submit();
+		AssertJUnit.assertEquals(HomePage.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
 	}
 	
 	@Test(description = "Login fail with blank pw")
 	public void Blank_pw() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		HomePage home = new HomePage(driver);
-		home.typeUsername("admin");
-		home.typePassword("");
-		home.submit();
-		AssertJUnit.assertEquals(home.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
+//		HomePage home = new HomePage(driver);
+//		home.typeUsername("admin");
+//		home.typePassword("");
+//		home.submit();
+		HomePage.typeUsername("admin");
+		HomePage.typePassword("");
+		HomePage.submit();
+		AssertJUnit.assertEquals(HomePage.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
 	}
 	@Test(description = "Login fail with invalid pw")
 	public void Invalid_pw() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		HomePage home = new HomePage(driver);
-		home.typeUsername("admin");
-		home.typePassword("admin1");
-		home.submit();
-		AssertJUnit.assertEquals(home.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
+//		HomePage home = new HomePage(driver);
+//		home.typeUsername("admin");
+//		home.typePassword("admin1");
+//		home.submit();
+		HomePage.typeUsername("admin");
+		HomePage.typePassword("admin1");
+		HomePage.submit();
+		AssertJUnit.assertEquals(HomePage.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
 	}
 	
 	@Test(description = "Login fail with unexisted acc")
 	public void Unexisted_acc() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		HomePage home = new HomePage(driver);
-		home.typeUsername("abc");
-		home.typePassword("admin");
-		home.submit();
-		AssertJUnit.assertEquals(home.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
+//		HomePage home = new HomePage(driver);
+//		home.typeUsername("abc");
+//		home.typePassword("admin");
+//		home.submit();
+		HomePage.typeUsername("abc");
+		HomePage.typePassword("admin");
+		HomePage.submit();
+		AssertJUnit.assertEquals(HomePage.getMessage1().contentEquals("ログイン名またはパスワードが違います。もう一度入力して下さい。"), true);
 	}
 	@Test(description = "Login Success")
 	public void LoginSuccess() throws InterruptedException {
-		HomePage home = new HomePage(driver);
-		home.typeUsername("admin");
-		home.typePassword("admin");
-		home.submit();
+//		HomePage home = new HomePage(driver);
+//		home.typeUsername("admin");
+//		home.typePassword("admin");
+//		home.submit();
+		HomePage.typeUsername("admin");
+		HomePage.typePassword("admin");
+		HomePage.submit();
 		Thread.sleep(10000);
 		System.out.println(driver.getTitle());
-		System.out.println(home.getUsername());
+		System.out.println(HomePage.getUsername());
 //		home.userlist();
 //		home.user();
 	}
