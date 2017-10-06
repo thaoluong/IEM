f62725dc855bee024acbb96a1e0c505cb1475ac6
NTQ-PC\NTQ <luonghongthao92@gmail.com>
2017-10-06 14:25:54 +0700

    Login_try (old) excuted success Login (changed) LoginIEM_POM (New)

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -4,51 +4,51 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-	WebDriver driver;
+	static WebDriver driver;
 
-	By account = By.id("account-menu");
-	By login = By.xpath("//a[@id='login']/span/span");
-	By username = By.id("username");
-	By password = By.id("password");
-	By submit = By.xpath("//button[@type='submit']");
+	static By account = By.id("account-menu");
+	static By login = By.xpath("//a[@id='login']/span/span");
+	static By username = By.id("username");
+	static By password = By.id("password");
+	static By submit = By.xpath("//button[@type='submit']");
 //	By message1 = By.xpath("//*[contains(@class,'alert alert-danger alert-dismissible')]"); k đc
 //	By message1 = By.cssSelector("div..firepath-matching-node"); k đc
-	By message1 = By.xpath("//ngb-alert/div/div");
+	static By message1 = By.xpath("//ngb-alert/div/div");
 //	By message1 = By.xpath("//div[@role='alert']/div");
 
 	By userlist = By.xpath("//a[@id='management-menu']/span/i");
 	By user = By.xpath("//div[@id='navbarResponsive']/ul/li/ul/li/a/span/span");
-	By username1 = By.xpath("//a[@id='account-menu']/span/span");
+	static By username1 = By.xpath("//a[@id='account-menu']/span/span");
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 	}
 
-	public void account() {
+	public static void account() {
 		driver.findElement(account).click();
 	}
 
-	public void login() {
+	public static void login() {
 		driver.findElement(login).click();
 	}
 
-	public void typeUsername(String uid) {
+	public static void typeUsername(String uid) {
 		driver.findElement(username).sendKeys(uid);
 	}
 
-	public void typePassword(String pw) {
+	public static void typePassword(String pw) {
 		driver.findElement(password).sendKeys(pw);
 	}
 	
-	public void submit() {
+	public static void submit() {
 		driver.findElement(submit).click();
 	}
 	
-	public String getMessage1(){
+	public static String getMessage1(){
 		return driver.findElement(message1).getText();
 	}
 	
-	public String getUsername(){
+	public static String getUsername(){
 		return driver.findElement(username1).getText();
 	}
 	
